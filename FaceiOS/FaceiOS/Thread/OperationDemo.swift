7d4ffc235fb6feb3b093d9bcb935181082ec5143
shan <1096452045@qq.com>
2020-09-14 14:16:11 +0800

    init

---
+++ FaceiOS/FaceiOS/Thread/OperationDemo.swift
@@ -0,0 +1,19 @@
+//
+//  OperationDemo.swift
+//  FaceiOS
+//
+//  Created by 山神 on 2020/5/28.
+//  Copyright © 2020 山神. All rights reserved.
+//
+
+import UIKit
+
+class OperationDemo {
+
+    func startOperationTool() {
+        OperationQueue
+
+
+    }
+
+}
